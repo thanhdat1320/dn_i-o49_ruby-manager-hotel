eee180aacc9083e32ef8a3bca37773eba2880659
huytn-1183 <tran.ngoc.huy@sun-asterisk.com>
2021-11-05 09:27:44 +0700

    OK

--- app/controllers/staff/rooms_controller.rb
+++ app/controllers/staff/rooms_controller.rb
@@ -1,12 +1,29 @@
 class Staff::RoomsController < ApplicationController
   before_action :load_rooms, only: %i(index)
+  before_action :load_room, only: %i(update)
+
   def index; end
 
+  def update
+    @room.available!
+    flash[:success] = t :success
+    redirect_to staff_rooms_path
+  end
+
+  private
+
   def load_rooms
-    @rooms = Room.pagination_at(filter_params[:page])
+    @rooms = Room.unavailable.pagination_at(filter_params[:page])
     return if @rooms.any?
 
     flash.now[:danger] = t :empty
+  end
+
+  def load_room
+    @room = Room.find params[:id]
+    return if @room
+
+    flash.now[:warning] = t :not_found
   end
 
   # params permit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :staff do
-    get 'rooms/index'
-  end
   scope "(:locale)", locale: /en|vi/ do
     root :to => "rooms#index"
     get :confirm, to: "rooms#confirm"
@@ -26,7 +23,7 @@ Rails.application.routes.draw do
 
     namespace :staff do
       resources :bookings, only: [:index, :update]
-      resources :rooms, only: :index
+      resources :rooms, only: [:index, :update]
     end
   end
 end
